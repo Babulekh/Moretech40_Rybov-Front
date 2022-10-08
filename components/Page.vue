383b701e5e09101d7a2bf674cff4f2326f9acfe7
thevostle <technic.dima@gmail.com>
2022-10-08 16:36:03 +0300

    компонент Page

--- components/Page.vue
+++ components/Page.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="v-page">
+        <Header />
         <slot></slot>
+        <Footer />
     </div>
 </template>
 
 <script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
 export default {
-
+    components: { Header, Footer }
 }
 </script>
