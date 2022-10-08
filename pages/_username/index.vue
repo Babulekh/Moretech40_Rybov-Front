140a17bcc5f16156396bb13b0bfe6d40234eede0
vano23201 <vanca624@gmail.com>
2022-10-08 20:38:38 +0300

    Path fix 2

---
+++ pages/_username/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Page>
+    <UserProfile :username="usernameParam" />
+  </Page>
+</template>
+
+<script>
+import Page from '@/components/Page.vue';
+import UserProfile from '@/components/UserProfile.vue';
+
+
+export default {
+  name: 'UserPage',
+  components: {
+    Page,
+    UserProfile
+  },
+  async asyncData({ params }) {
+    return {
+      usernameParam: params.username,
+    };
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
